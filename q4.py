258ff0edbc7b216ccb1801826ff7d8af2083901a
Devan <devankav7@gmail.com>
2020-11-10 23:31:39 -0500

    deleted old files

--- q4.py
+++ q4.py
@@ -20,3 +20,35 @@ def isPalindrome(s):
 #work backwards
 #find the largest palindromes and then find its factors.
 #if the factors are 2 3-digit numbers than shes a go.
+
+#divide n by 2 and make it an int
+#keep lower this until
+
+# i have no idea what im doing :)
+
+def factors(n):
+    f = []
+
+    print("n is: " + str(n))
+
+    if n > 2:
+        i = int(n/2)
+
+        while n%i != 0:
+            print(i)
+            i-=1
+        
+        f.append(i)
+
+        r = factors(i)
+        for x in range(0, len(r)):
+            f.append(r[x])
+    
+    if n%3 == 0 and int(n/3) not in f:
+        f.append(int(n/3))
+    
+    return f
+
+test = factors(int(input()))
+print(test)
+print("There are " + str(len(test)) + " factors")
