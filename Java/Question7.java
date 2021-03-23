093bc459b00d640fa99cb8c1adffbb028f6cbb6a
Devan Kavalchek <devankav7@gmail.com>
2021-03-22 21:39:17 -0400

    updated q7

--- Java/Question7.java
+++ Java/Question7.java
@@ -2,7 +2,7 @@ package Java;
 
 public class Question7 {
     public static void main(String[] args) {
-        NthPrime sieve = new NthPrime(10001);
+        NthPrime sieve = new NthPrime(100001);
         int target = sieve.getTarget();
 
         System.out.println(target);
